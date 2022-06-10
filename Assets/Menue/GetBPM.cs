cf615a0283f5f8015eb9b83c0ff8085c6f55b94a
CarinaKt <63025657+CarinaKt@users.noreply.github.com>
2022-06-10 13:54:40 +0200

    Fix knob fill

--- Assets/Menue/GetBPM.cs
+++ Assets/Menue/GetBPM.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
@@ -11,28 +12,22 @@ public class GetBPM : MonoBehaviour
     [SerializeField] private float  rotation;
     [SerializeField] private Image fill;
     [SerializeField] private Text valText;
+
+    [SerializeField] private int maxAngle = 70;
     // Start is called before the first frame update
 
     // Update is called once per frame
     void Update()
-    { 
-        rotation = this.transform.rotation.z * Mathf.Rad2Deg ;
-        angle = this.transform.rotation.z * Mathf.Rad2Deg ;
-        angle = (angle <= 0) ? (360 + angle) : angle;
+    {
+        rotation = this.transform.rotation.z * Mathf.Rad2Deg - 39 ;
+        angle = this.transform.rotation.z * Mathf.Rad2Deg - 39 ;
         
-        if (angle<=225 || angle>=315)
-        {
-            float r = angle + 135f;
-            rotation = r;
-            angle = ((angle >= 315) ? (angle - 360) : angle) + 45;
-            fill.fillAmount = 0.75f - (angle / 360f); 
-            valText.text = Mathf.Round((fill.fillAmount * 100) / 0.75f).ToString(CultureInfo.InvariantCulture);
-        }
+        angle = angle < 0 ? angle * -1 : angle;
+        int angleRound = (int)angle;
         
+        fill.fillAmount = ((float)angleRound/ maxAngle) + 0.014f;
+        valText.text = angleRound.ToString(CultureInfo.InvariantCulture);
         
-        //valText.text = angle.ToString(CultureInfo.InvariantCulture);
-        //fill.fillAmount = angle * 100;
-
     }
 }
 
